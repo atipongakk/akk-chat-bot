6229cbd23af507a309d447a5df828acfdc4fec33
Atipong Akkarasa <atipongakk@gmail.com>
2017-06-27 00:35:29 +0700

    test

--- bot.php
+++ bot.php
@@ -55,7 +55,41 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Thank for submit images'
+				'text' => 'Thank for submit images [' . $messageId . ']'
+			];
+			
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+		//Reply only when message sent is in 'image' format
+		else if($event['type'] == 'message' && $event['message']['type'] == 'audio'){
+			// Get message Id
+			$messageId = $event['message']['id'];
+			
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => 'Thank for submit audio [' . $messageId . ']'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
